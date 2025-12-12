0613cf2a7db05a3de9448777c1a7a6e67d560273
satetsu888 <satetsu888@gmail.com>
2025-12-12 16:43:27 +0900

    Add address to confirm params

--- types/payments-js/setup-flows.d.ts
+++ types/payments-js/setup-flows.d.ts
@@ -1,4 +1,4 @@
-import { PayjpPaymentsError } from '..';
+import { Address, PayjpPaymentsError } from '..';
 
 export type ConfirmSetupParams = {
     return_url: string;
@@ -7,6 +7,7 @@ export type ConfirmSetupParams = {
             name?: string;
             email?: string;
             phone?: string;
+            address?: Address;
         };
     };
 }
